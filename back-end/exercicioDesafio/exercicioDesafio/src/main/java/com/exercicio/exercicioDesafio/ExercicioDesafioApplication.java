85aa275732f1906db576ae15454e2660a0aadde9
elizabetholiveira <elizabeth.oliveira@zup.com.br>
2023-09-15 15:48:54 -0300

    commit inicial

---
+++ back-end/exercicioDesafio/exercicioDesafio/src/main/java/com/exercicio/exercicioDesafio/ExercicioDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.exercicioDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioDesafioApplication.class, args);
+	}
+
+}
---
+++ back-end/exercicioDesafio/exercicioDesafio/src/test/java/com/exercicio/exercicioDesafio/ExercicioDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.exercicioDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
